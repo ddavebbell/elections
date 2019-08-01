ffc6de50729556ccb1082f8acf4914ac02b0310b
somethinrother <jamesneilson4@gmail.com>
2019-08-01 16:52:55 -0400

    Small cleanup

--- election.js
+++ election.js
@@ -9,8 +9,6 @@ document.addEventListener("DOMContentLoaded", async function() {
 		let submit = submitInputTag(candidate)
 
 		form.addEventListener('submit', function(event){
-			// event.preventDefault();
-
 			$.ajax({
 				url: 'http://localhost:3000/votes',
 				method: 'POST',
@@ -84,27 +82,3 @@ function submitInputTag(candidate) {
 	submit.setAttribute('type', 'submit');
 	return submit;
 }
-
-// candidate = `<li>Name: ${response.candidates[i].name}<br>Votes: ${response.candidates[i].votes}
-// <form id="form${i}" action="POST" action=action="https://bb-election-api.herokuapp.com/vote">
-// <input type="submit" value="Vote!">
-// <input type="hidden" name=${response.candidates[i].name} value=${response.candidates[i].name}>
-// </form>
-// </li><br>`;
-// election.insertAdjacentHTML('beforeend',candidate)
-//
-// var forms = document.querySelectorAll('form')
-//
-// forms[i].addEventListener('submit',function(e){
-// 	e.preventDefault();
-// 	$.ajax({
-// 		url: 'https://bb-election-api.herokuapp.com/vote',
-// 		method: 'POST',
-// 		dataType: 'JSON',
-// 		data: {name: this.querySelector('input[type=hidden]').value}
-// 	}).done(function(){
-// 		console.log(`Vote submitted!`)
-// 	}).fail(function(){
-// 		console.log('Voted Failed!')
-// 	})
-// });
